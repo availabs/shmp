79fa089fcd45113e576bae20911675b74a0be150
Alex Muro <am3081@gmail.com>
2021-03-02 21:05:26 -0500

    no dms

--- src/components/dms/components/dms-content.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const DmsContent = ({ className="mt-20 flex-1 w-full mx-auto max-w-7xl mb-10", children }) =>
-  <div className={ className }>
-    { children }
-  </div>
-export default DmsContent;
--- src/components/dms/contexts/messenger-context.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const MessengerContext = React.createContext({
-  pageMessages: [], attributeMessages: []
-});
-
-export const useMessenger = () => React.useContext(MessengerContext);
-
-export default MessengerContext;
