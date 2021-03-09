042713f25b142f488966d061c97e713709a4b54b
shaunaksangdod <shaunaksangdod@gmail.com>
2021-03-09 13:54:44 -0500

    drft issue saving

--- src/pages/Admin/Meta/components/Element/draft-editor/index.js
+++ src/pages/Admin/Meta/components/Element/draft-editor/index.js
@@ -1,8 +1,16 @@
 import React from "react"
+import get from 'lodash.get'
 // import { useTheme } from "components/avl-components/wrappers/with-theme"
-import Editor, { createEmpty } from "components/dms/components/editor"
+import Editor, { createEmpty, createWithContent, createEditorState } from "components/dms/components/editor"
 import ReadOnlyEditor from "components/dms/components/editor/editor.read-only"
+import htmlToDraft from 'html-to-draftjs';
 
+import {
+    convertToRaw,
+    convertFromRaw,
+    ContentState,
+    EditorState
+} from 'draft-js';
 function isJson(str) {
     try {
         JSON.parse(str);
@@ -14,14 +22,21 @@ function isJson(str) {
 
 
 const Edit = ({value, onChange}) => {
-    let data = value && isJson(value) ? JSON.parse(value) : createEmpty()
+    let data = value ? isJson(value) ? JSON.parse(value) : value : createEmpty()
+    console.log('in edit of draft', data, 'value', value)
 
     return (
         <div className='w-full'>
             <div className='relative'>
                 Draft Editor
                 <Editor 
-                    value ={ data }
+                    value ={ !value ? createEmpty() : EditorState.createWithContent(data) }
+                    //onChange={(e) => onChange(JSON.stringify(convertToRaw(e.getCurrentContent())))}
+                    onChange={(e) => {
+                        console.log('onChange', e, JSON.stringify(convertToRaw(e.getCurrentContent())))
+                        //onChange(JSON.stringify(e.getCurrentContent()))
+                        onChange(JSON.stringify(convertToRaw(e.getCurrentContent())))
+                    }}
                 />
             </div>
         </div>
@@ -34,18 +49,20 @@ Edit.settings = {
 }
 
 const View = ({value}) => {
+    return <div></div>
     if (!value) return false
-    let data = value['element-data']
+    let data = convertToRaw(value['element-data'].getCurrentContent())  // convertToRaw(value['element-data'])
+    console.log('in view of draft', data )
     return (
         <div className='relative w-full border border-dashed p-1'>
-            <ReadOnlyEditor value={value.content} isRaw={value.content.blocks ? true : false} />
+            <ReadOnlyEditor value={JSON.stringify(data)} isRaw={!!get(data, ['blocks'])} />
         </div>
     )
 }
 
 
 export default {
-    "name": 'Draft Editor',
+    "name": 'DraftEditor',
     "edit": Edit,
     "view": View
 }
