ee3e82f6e6de030553c4e5a827e1fa9fe6aaa4e8
shaunaksangdod <shaunaksangdod@gmail.com>
2021-03-25 09:34:15 -0400

    basic assets table - with risk data

--- src/pages/Admin/Meta/components/Element/AssetsTable/AssetsTable.js
+++ src/pages/Admin/Meta/components/Element/AssetsTable/AssetsTable.js
@@ -9,7 +9,9 @@ const nameMapping = {
     'Owner Type': 'ownerType',
     'Land Use Type': 'propType',
     'County': 'counties',
-    'Municipality': 'municipalities'
+    'Municipality': 'municipalities',
+    'Annual 1%': '100 yr',
+    'Annual 0.2%': '500 yr',
 }
 
 const keyCols = ['Jurisdiction', 'Owner Type', 'Land Use Type'];
@@ -76,7 +78,7 @@ function renderMetaOptions(props, state, setState, cache) {
             <label> Select Columns: </label>
             <Select
                 key={'cols'}
-                domain={[state.groupBy, 'Number of Assets', 'Value of Assets', ' # in 100 yr', '$ in 100 yr', '# in 500 yr', '$ in 500 yr']}
+                domain={[state.groupBy, 'Number of Assets', 'Value of Assets', '# in 100 yr', '$ in 100 yr', '# in 500 yr', '$ in 500 yr']}
                 value={_.uniqBy([state.groupBy, ...state.cols])}
                 onChange={e => handleChange(Object.assign({}, state, {cols: e}))}
             />
@@ -101,23 +103,53 @@ function processData(state, cache) {
 
     if (!geoGraph || !geoGraph.length) return {data, columns};
 
-    let graph = get(cache, ['building', 'byGeoid', activeGeo, nameMapping[state.groupBy]], {})
+    let graph = get(cache, ['building', 'byGeoid', activeGeo, nameMapping[state.groupBy]], {});
+    let riskMappings = _.get(cache, ['building', 'byGeoid', 36, nameMapping[state.groupBy]], {})
 
     data = (state.groupBy === 'Jurisdiction' ? geoGraph : filterByTypes.getValues(filterByTypes[state.groupBy]))
         .filter(gbvKey => state.filterByValue.includes(gbvKey) || !state.filterByValue.length)
         .reduce((a, gbvKey) => {
+
             let newKey = state.groupBy === 'Land Use Type' ? gbvKey - (gbvKey % 100) : gbvKey;
 
-            a[newKey] = {
-                [state.groupBy]: state.groupBy === 'Jurisdiction' ? get(cache, ['geo', newKey, 'name']) : filterByTypes.getName(filterByTypes[state.groupBy], newKey.toString()),
-                'Number of Assets': get(a, [newKey, 'Number of Assets'], 0) + parseInt(get(graph, [gbvKey, 'sum', 'count', 'value'], 0)),
-                'Value of Assets': get(a, [newKey, 'Value of Assets'], 0) + parseInt(get(graph, [gbvKey, 'sum', 'replacement_value', 'value'], 0))
-            }
-            return a
-        }, {})
-    data = Object.keys(data)
-        .map(gbvKey => data[gbvKey])
+            let riskData =
+                _.map(_.values(_.get(riskMappings, [gbvKey, 'byRiskScenario'], {})),
+                    (d) => _.get(d, ['byRiskZone', 'all', 'value']))
+                    .filter(c => c)
+                    .reduce((rdA, rdC) => {
+                        rdC.forEach(c1 => {
+                            rdA[`$ in ${nameMapping[c1.name]}`] = get(rdA, `$ in ${nameMapping[c1.name]}`, 0) + parseInt(c1.sum || '0');
+                            rdA[`# in ${nameMapping[c1.name]}`] = get(rdA, `# in ${nameMapping[c1.name]}`, 0) + parseInt(c1.count || '0')
+                        })
+                        return rdA
+                    }, {})
 
+            let tmp = {
+                [state.groupBy]:
+                    state.groupBy === 'Jurisdiction' ?
+                        get(cache, ['geo', newKey, 'name']) :
+                        filterByTypes.getName(filterByTypes[state.groupBy], newKey.toString()),
+                'Number of Assets': get(a, [newKey, 'Number of Assets'], 0) + parseInt(get(graph, [gbvKey, 'sum', 'count', 'value'], 0)),
+                'Value of Assets': get(a, [newKey, 'Value of Assets'], 0) + parseInt(get(graph, [gbvKey, 'sum', 'replacement_value', 'value'], 0)),
+                ...Object.keys(riskData).reduce((riskZoneDataA, riskZoneDataKeyC) => {
+                    riskZoneDataA[riskZoneDataKeyC] = get(a, [newKey, riskZoneDataKeyC], 0) + (riskData[riskZoneDataKeyC] || 0);
+                    return riskZoneDataA
+                }, {})
+            }
+            a[newKey] = Object.assign({}, a[newKey], tmp)
+            return a
+        }, {});
+
+    data = Object.keys(data).map(gbvKey => data[gbvKey]);
+
+    data.push(
+        data.reduce((total, current) => {
+            _.keys(current)
+                .filter(key => key !== state.groupBy)
+                .forEach(key => total[key] = get(total, [key], 0) + current[key])
+            return total;
+        }, {[state.groupBy]: 'Total'})
+    )
 
     state.cols.forEach(column => {
             columns.push({
@@ -143,7 +175,7 @@ function AssetsTable(props) {
         cols: [],
         filterBy: null,
         filterByValue: [],
-        groupBy: 'Owner Type',
+        groupBy: 'Jurisdiction',
         pageSize: null,
     }
     const [state, setState] =
@@ -163,24 +195,20 @@ function AssetsTable(props) {
 
         async function fetchData() {
             let response = await falcor.get(
-                ["geo", activeGeo, childGeo],
-                ['plan', activePlan, 'scenarios']
+                ["geo", activeGeo, childGeo]
             )
 
             response = get(response, ['json', 'geo', '36', childGeo], []);
-            let scenarios = get(response, ['json', 'plan', activePlan, 'scenarios'], []);
+            let scenarios = [3, 4, 9, 10, 38, 12, 14, 15, 16, 40, 18, 19, 41, 43, 22, 44, 23, 24, 25, 26, 46, 28, 29, 47,
+                30, 49, 31, 52, 20, 27, 17, 33, 34, 13, 32, 42, 36, 35, 53, 54, 55, 56]; // Only DFIRMs
+
             let groupByValues = state.groupBy === 'Jurisdiction' ? response : filterByTypes.getValues(filterByTypes[state.groupBy]);
 
             let reqs = [
                 ['geo', [activeGeo, ...response], ['name']],
                 ['building', 'byGeoid', activeGeo, nameMapping[state.groupBy], groupByValues, 'sum', ['count', 'replacement_value']],
+                ['building', 'byGeoid', activeGeo, nameMapping[state.groupBy], groupByValues, 'byRiskScenario', scenarios, 'byRiskZone', 'all']
             ];
-
-            if (scenarios) {
-                scenarios = scenarios.filter(f => f.name.includes('DFIRM')).map(f => f.id);
-                reqs.push(['building', 'byGeoid', activeGeo, nameMapping[state.groupBy], groupByValues, 'byRiskScenario', scenarios, 'byRiskZone', 'all'])
-                console.log('sc?', scenarios)
-            }
             if (response.length) {
                 await falcor.get(
                     ...reqs
@@ -190,7 +218,7 @@ function AssetsTable(props) {
 
         return fetchData();
     }, [childGeo, falcor, falcorCache, state]);
-    console.log('cache?', falcorCache)
+
     return (
         <div>
             {props.viewOnly ? null : renderMetaOptions(props, state, setState, falcorCache)}
